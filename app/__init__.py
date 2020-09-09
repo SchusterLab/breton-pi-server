e9ba93fcec58d7a8b791e31d528fff7abff809dc
grangerave <grangerave@yahoo.com>
2020-09-09 11:11:58 -0500

    removed garbage files

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask#, url_for
-from flask_reverse_proxy_fix.middleware import ReverseProxyPrefixFix
-
-app = Flask(__name__)
-app.config['REVERSE_PROXY_PATH'] = '/flask'
-ReverseProxyPrefixFix(app)
-
-from app import views
